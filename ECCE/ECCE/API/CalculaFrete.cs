95db3899334c60e55e457e84b0d9a27dd7f27b01
danieltoguti <danieltoguti@gmail.com>
2021-03-27 11:15:07 -0300

    Api Frete

---
+++ ECCE/ECCE/API/CalculaFrete.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ECCE.API
+{
+    public class CalculaFrete
+    {       
+        public async Task<string> GetApi(string CepDestino, string CdServico)
+        {            
+            var CalcPrecoPrazo = new API_Correios.CalcPrecoPrazoWSSoapClient(API_Correios.CalcPrecoPrazoWSSoapClient.EndpointConfiguration.CalcPrecoPrazoWSSoap12);
+            var resp=await  CalcPrecoPrazo.CalcPrecoAsync("", "", CdServico, "11790000", CepDestino, "2", 1, 20, 20, 20, 0, "N", 0, "N");
+            try
+            {
+                if (resp.Servicos.Count() > 0)
+                {
+                    return resp.Servicos.FirstOrDefault().Valor;
+                }
+                else {
+                    return "Verifique se o cep está correto!";
+                }
+            }
+            catch {
+                return "Erro ao Buscar";
+            }
+
+
+
+
+        }
+      
+    }
+
+
+
+
+}
---
+++ ECCE/ECCE/Controllers/CalculaFreteController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECCE.API;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EECE.Controllers
+{
+
+    public class CalculaFreteController : Controller
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> GetCalculo(string cep, string cdservico)
+        {
+
+            if (String.IsNullOrEmpty(cep))
+            {
+                return Json(new { status = false, resposta = "Infome um cep válido" });
+            }
+
+            CalculaFrete api = new CalculaFrete();                        
+            var resp = await api.GetApi(cep, cdservico);
+            return Json(new { status = true, resposta = resp });
+        }
+
+    }
+}
